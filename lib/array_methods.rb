f966484439177aae8a9af1c98764e4934e228388
Ashley Winters <ashbwinters@gmail.com>
2020-03-13 21:51:09 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,10 +4,10 @@ end
 
 def find_max_value(array)
   sorted_array = array.sort
-  puts sorted_array[-1]
+  sorted_array.last
 end
 
 def find_min_value(array)
   sorted_array = array.sort
-  puts sorted_array[0]
+  sorted_array.first
 end
